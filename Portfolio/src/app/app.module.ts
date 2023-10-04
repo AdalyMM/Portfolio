f15add984d9e81d9abc8a232cc43f65d4915ba51
AdalyMoncada <desarrollotemporal@gmail.com>
2023-10-04 03:00:22 -0600

    Adding the navbar and updating the responsive views. The views are unfinished.

--- Portfolio/src/app/app.module.ts
+++ Portfolio/src/app/app.module.ts
@@ -1,20 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { MainPageComponent } from './pages/main-page/main-page.component';
+import { AboutMePageComponent } from './pages/about-me-page/about-me-page.component';
+import { ProjectsPageComponent } from './pages/projects-page/projects-page.component';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    MainPageComponent
+    MainPageComponent,
+    AboutMePageComponent,
+    ProjectsPageComponent,
+    ContactPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([])
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Portfolio/src/app/pages/main-page/main-page.component.ts
+++ Portfolio/src/app/pages/main-page/main-page.component.ts
@@ -1,13 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit } from '@angular/core';
 import { SharedDataService } from 'src/app/services/shared-data.service';
 import { Breakpoints } from '@angular/cdk/layout';
+import { gsap } from 'gsap';
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.sass']
 })
-export class MainPageComponent implements OnInit {
+export class MainPageComponent implements OnInit, AfterViewInit {
   breakpoints: { [key: string]: boolean } = {
     XSmall: false,
     Small: false,
@@ -15,22 +16,88 @@ export class MainPageComponent implements OnInit {
     Large: false,
     XLarge: false,
   };
-  isSmall = false;
 
-  constructor(private sharedService: SharedDataService) {}
+  constructor(
+    private sharedDataService: SharedDataService,
+    private elementRef: ElementRef) {}
 
   ngOnInit() {
-    this.sharedService.currentBreakpoints.subscribe((values) => {
+    this.sharedDataService.currentBreakpoints.subscribe((values) => {
       this.breakpoints['XSmall'] = values[Breakpoints.XSmall];
       this.breakpoints['Small'] = values[Breakpoints.Small];
       this.breakpoints['Medium'] = values[Breakpoints.Medium];
       this.breakpoints['Large'] = values[Breakpoints.Large];
       this.breakpoints['XLarge'] = values[Breakpoints.XLarge];
-      this.isSmall = values[Breakpoints.Medium];
-      console.log(this.breakpoints);
-      console.log(this.isSmall);
     });
-    console.log(this.breakpoints['XSmall']);
   }
 
+  ngAfterViewInit() {
+    // Select the elements to rotate.
+    const boxUpperHexagon = document.querySelector('.box-upper-hexagon');
+    const boxLittleHexagon1 = document.querySelector('.box-little-hexagon-1');
+    const boxLittleHexagon2 = document.querySelector('.box-little-hexagon-2');
+    const boxLittleHexagon3 = document.querySelector('.box-little-hexagon-3');
+    const boxLittleHexagon4 = document.querySelector('.box-little-hexagon-4');
+    const boxLowerHexagon = document.querySelector('.box-lower-hexagon');
+
+    // Animation for rotate hexagons.
+    gsap.to([boxUpperHexagon, boxLittleHexagon1, boxLittleHexagon2, boxLittleHexagon3, boxLittleHexagon4,boxLowerHexagon], {
+      rotation: 360, // Rotate 360 degrees.
+      duration: 10,   // Animation duration in seconds.
+      repeat: -1,    // Infinite loop.
+      ease: 'linear', // Ease function for constant rotation.
+    });
+    this.mainTitleAnimation();
+  }
+
+  // Animation for the main title.
+  mainTitleAnimation(): void {
+    const h1 = this.elementRef.nativeElement.querySelector('h1');
+    const letras = h1.textContent.split('');
+    let h1AnimationEnd = false;
+
+    h1.textContent = '';
+
+    letras.forEach((letra: string | null, index: number) => {
+      const span = document.createElement('span');
+      span.textContent = letra;
+      span.style.opacity = '0';
+      h1.appendChild(span);
+
+      gsap.to(span, {
+        opacity: 1,
+        duration: 0.1,
+        ease: 'power2.inOut',
+        delay: index * 0.1,
+        onComplete: (() => {
+          if(index === 14){
+            this.mainSubtitleAnimation();
+          }
+        })
+      });
+    });
+  }
+
+  // Animation for the main subtitle
+  mainSubtitleAnimation(): void {
+    const h2 = this.elementRef.nativeElement.querySelector('h2');
+    const letras = h2.textContent.split('');
+    h2.style.opacity = '1'
+
+    h2.textContent = '';
+
+    letras.forEach((letra: string | null, index: number) => {
+      const span = document.createElement('span');
+      span.textContent = letra;
+      span.style.opacity = '0';
+      h2.appendChild(span);
+
+      gsap.to(span, {
+        opacity: 1,
+        duration: 0.05,
+        ease: 'power2.inOut',
+        delay: index * 0.1,
+      });
+    });
+  }
 }
